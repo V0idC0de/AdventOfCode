dbe4a3d2b2f68e8673683227aad5e3dffa416266
V0idC0de <26016825+V0idC0de@users.noreply.github.com>
2023-12-07 01:14:23 +0100

    Added alterantive approach for Day 4 part 2

--- day4.py
+++ day4.py
@@ -1,6 +1,6 @@
-import itertools
 import re
-from typing import NamedTuple
+import time
+from typing import NamedTuple, Iterator
 
 with open(f'{__file__.split(".")[0]}.txt') as f:
     input_lines = [l.strip() for l in f.readlines()]
@@ -51,4 +51,26 @@ def count_cards(card_stack: list[Card]) -> int:
     return 1 + count_cards(won_duplicates) + count_cards(card_stack[1:])
 
 
-print(f"Challenge 2: {count_cards(cards)}")
+start_time = time.time_ns()
+amount_cards = count_cards(cards)
+print(f"Challenge 2 (simple solution in {(time.time_ns() - start_time) / 10 ** 9:.2f} sec):\t{amount_cards}")
+
+
+# Part 2
+# This alternative approach avoids constructing lists directly and tries to use generators instead.
+# Interestingly enough, the simple timing test reveals that the generator-based approach is ~40% slower.
+def count_cards(card_stack: Iterator[Card]) -> int:
+    try:
+        current_card = next(card_stack)
+    except StopIteration:
+        return 0
+    # Generates indices of the won duplicate cards, by taking the number of the current card and using it as an index
+    # (it's 1-indexed, so no need to add 1) and proceeding until the amount of wins the top-most card has is reached.
+    duplicate_indices = range(current_card.num, current_card.num + current_card.wins)
+    won_duplicates = (cards[i] for i in duplicate_indices)
+    return 1 + count_cards(won_duplicates) + count_cards(card_stack)
+
+
+start_time = time.time_ns()
+amount_cards = count_cards(iter(cards))
+print(f"Challenge 2 (generator-based in {(time.time_ns() - start_time) / 10 ** 9:.2f} sec):\t{amount_cards}")
